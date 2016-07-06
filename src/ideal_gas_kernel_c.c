6860351b93eb4f2528235f80d5dc7bbad57e427c
Jam <jamessewart@gmail.com>
2016-07-06 14:53:17 +0100

    first kokkos parallel loop

--- src/ideal_gas_kernel_c.c
+++ src/ideal_gas_kernel_c.c
@@ -26,38 +26,38 @@
 #include <stdlib.h>
 #include "ftocmacros.h"
 #include <math.h>
+#include <Kokkos_Core.hpp>
 
-void ideal_gas_kernel_c_(int *xmin,int *xmax,int *ymin,int *ymax,
-                      double *density,
-                      double *energy,
-                      double *pressure,
-                      double *soundspeed)
+using namespace Kokkos;
+
+void ideal_gas_kernel_c_(int *xmin, int *xmax, int *ymin, int *ymax,
+                         double *density,
+                         double *energy,
+                         double *pressure,
+                         double *soundspeed)
 {
-  int x_min=*xmin;
-  int x_max=*xmax;
-  int y_min=*ymin;
-  int y_max=*ymax;
+    int x_min = *xmin;
+    int x_max = *xmax;
+    int y_min = *ymin;
+    int y_max = *ymax;
 
-  int j,k;
-
-  double sound_speed_squared,v,pressurebyenergy,pressurebyvolume;
-  
-
-
-
-  for (k=y_min;k<=y_max;k++) {
+    parallel_for(y_max - y_min, KOKKOS_LAMBDA (const int& i) {
+        int k = i + y_min;
 #pragma ivdep
-    for (j=x_min;j<=x_max;j++) {								 
-      v=1.0/density[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)];
-      pressure[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)]=(1.4-1.0)*density[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)]
-                                                                   *energy[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)];
-      pressurebyenergy=(1.4-1.0)*density[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)];
-      pressurebyvolume=-density[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)]*pressure[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)];
-      sound_speed_squared=v*v*(pressure[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)]*pressurebyenergy-pressurebyvolume);
-      soundspeed[FTNREF2D(j  ,k  ,x_max+4,x_min-2,y_min-2)]=sqrt(sound_speed_squared);
-    }
-  }
+        for (int j = x_min; j <= x_max; j++) {
+            double v = 1.0 / density[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)];
 
+            pressure[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)] =
+                (1.4 - 1.0) * density[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)]
+                * energy[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)];
 
+            double pressurebyenergy = (1.4 - 1.0) * density[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)];
 
+            double pressurebyvolume = -density[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)] * pressure[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)];
+
+            double sound_speed_squared = v * v * (pressure[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)] * pressurebyenergy - pressurebyvolume);
+
+            soundspeed[FTNREF2D(j  , k  , x_max + 4, x_min - 2, y_min - 2)] = sqrt(sound_speed_squared);
+        }
+    });
 }
