bd5e185ab287786e34fe1ccadfe08727650f61aa
Jam <jamessewart@gmail.com>
2016-07-06 14:49:56 +0100

    add kokkos initialisation and finalize

--- src/clover_leaf.c
+++ src/clover_leaf.c
@@ -4,15 +4,18 @@
 #include "hydro.h"
 #include <mpi.h>
 #include "clover.h"
-
+#include <Kokkos_Core.hpp>
 
 
 int main(int argc, char **argv)
 {
+    Kokkos::initialize(argc, argv);
     clover_init_comms(argc, argv);
     BOSSPRINT(stdout, "\nClover Version %8.3f\n", g_version);
+    BOSSPRINT(stdout, "Task Count %d\n", parallel.max_task);
     initialise();
     hydro();
 
+    Kokkos::finalize();
     MPI_Finalize();
 }
